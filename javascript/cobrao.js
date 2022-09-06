d291831162f5701289b79f1d01510dd64115ca19
Pedro Henrique Soares <phenriquesoares360@gmail.com>
2022-09-06 20:33:22 -0300

    Movimentação da cobra

--- javascript/cobrao.js
+++ javascript/cobrao.js
@@ -1,12 +1,46 @@
 let lastRenderTime = 0
-const cobra_velocidade = 2 //velocidade do jogo(cobra)
+const cobra_velocidade = 1 //velocidade do jogo(cobra)
+
+const corpoCobra= [
+    { x: 10, y: 10 },
+    { x: 11, y: 10 },
+    { x: 12, y: 10 },
+    { x: 13, y: 10 },
+    { x: 14, y: 10 }
+]
+
+const gameBoard = document.getElementById('jogo-cobra')
+
+function updateCobra(){
+    for (let i = corpoCobra.length - 2; i >= 0; i--) {
+        corpoCobra[i + 1] = { ...corpoCobra[i] }
+      }
+    
+      corpoCobra[0].x += 0 //quantidade de quadrados que a cobra vai se mexer por segundo na horizontal
+      corpoCobra[0].y += 1 //quantidade de quadrados que a cobra vai se mexer por segundo na vertical
+
+//função para a atualização da cobra(para ela se mexer)
+} 
+
+function drawnCobra(gameBoard){
+    corpoCobra.forEach(segment => {
+        const elementoDaCobra = document.createElement('div')
+        elementoDaCobra.style.gridRowStart = segment.x
+        elementoDaCobra.style.gridColumnStart = segment.y
+        elementoDaCobra.classList.add('cobra')
+        gameBoard.appendChild(elementoDaCobra)
+    });
+
+//função para o desenho da cobra
+} 
+
 
 function main(currentTime){//função para calcular o tempo e movimento do jogo
     window.requestAnimationFrame(main)
     const secondsSinceLastRender = (currentTime- lastRenderTime)/1000
-    if(secondsSinceLastRender < 1/ cobra_velocidade)return 
+    if(secondsSinceLastRender < 1 / cobra_velocidade) return 
    
-    console.log("render, ou seja deu certo")
+
     lastRenderTime = currentTime
     
     update()
@@ -15,10 +49,12 @@ function main(currentTime){//função para calcular o tempo e movimento do jogo
 
 window.requestAnimationFrame(main)
 
-function update(){
 
+function update(){
+updateCobra()
 }
 
-function drawn(){
-    
+function draw(){
+gameBoard.innerHTML =' '    
+drawnCobra(gameBoard)
 }

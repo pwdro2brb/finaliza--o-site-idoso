574dd4923b2ef9ad7b97e9c590e74bc72aa4f298
Pedro Henrique Soares <phenriquesoares360@gmail.com>
2022-09-08 18:31:09 -0300

    adicinol á movimentação da cobra com as setas

--- javascript/cobrao.js
+++ javascript/cobrao.js
@@ -1,26 +1,25 @@
 let lastRenderTime = 0
-const cobra_velocidade = 1 //velocidade do jogo(cobra)
-
+const cobra_velocidade = 2 //velocidade do jogo(cobra)
+let ultimaDireçaoInput = { x: 0, y: 0 }
 const corpoCobra= [
-    { x: 10, y: 10 },
-    { x: 11, y: 10 },
-    { x: 12, y: 10 },
-    { x: 13, y: 10 },
-    { x: 14, y: 10 }
+    { x: 11, y: 11 }
 ]
 
 const gameBoard = document.getElementById('jogo-cobra')
 
+
 function updateCobra(){
+    const direçaoInput = getDireçaoInput()
     for (let i = corpoCobra.length - 2; i >= 0; i--) {
         corpoCobra[i + 1] = { ...corpoCobra[i] }
       }
     
-      corpoCobra[0].x += 0 //quantidade de quadrados que a cobra vai se mexer por segundo na horizontal
-      corpoCobra[0].y += 1 //quantidade de quadrados que a cobra vai se mexer por segundo na vertical
+      corpoCobra[0].x += direçaoInput.x //quantidade de quadrados que a cobra vai se mexer por segundo na horizontal
+      corpoCobra[0].y += direçaoInput.y //quantidade de quadrados que a cobra vai se mexer por segundo na vertical
 
 //função para a atualização da cobra(para ela se mexer)
 } 
+
 
 function drawnCobra(gameBoard){
     corpoCobra.forEach(segment => {
@@ -35,12 +34,12 @@ function drawnCobra(gameBoard){
 } 
 
 
-function main(currentTime){//função para calcular o tempo e movimento do jogo
+function main(currentTime){
     window.requestAnimationFrame(main)
     const secondsSinceLastRender = (currentTime- lastRenderTime)/1000
     if(secondsSinceLastRender < 1 / cobra_velocidade) return 
    
-
+//função para calcular o tempo e movimento do jogo
     lastRenderTime = currentTime
     
     update()
@@ -49,6 +48,38 @@ function main(currentTime){//função para calcular o tempo e movimento do jogo
 
 window.requestAnimationFrame(main)
 
+
+let direçaoInput = { x: 0, y: 0 }
+
+window.addEventListener('keydown', e =>{
+    switch (e.key) {
+//seção de comando que faz apertar as setas do teclado para fazer a cobra movimentar
+        case 'ArrowUp':
+            if (ultimaDireçaoInput.x !==0)break
+            direçaoInput = { x: -1, y: 0}
+              break
+
+        case 'ArrowDown':
+            if (ultimaDireçaoInput.x !==0)break
+            direçaoInput = { x: 1, y: 0}
+              break
+
+        case 'ArrowLeft':
+            if (ultimaDireçaoInput.y !==0)break
+            direçaoInput = { x: 0, y: -1}
+              break
+
+        case 'ArrowRight':
+            if (ultimaDireçaoInput.y !==0)break
+            direçaoInput = { x: 0, y: 1}
+              break
+}
+})
+
+function getDireçaoInput() {
+ ultimaDireçaoInput = direçaoInput //serve para consertar o bug de a cobra poder dar ré(no jogo da cobra isso não existe)
+    return direçaoInput
+}
 
 function update(){
 updateCobra()

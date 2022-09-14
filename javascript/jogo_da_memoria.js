b285a72b95e426f2e5d46eb2e0a11a35fed33014
Pedro Henrique Soares <phenriquesoares360@gmail.com>
2022-09-14 19:50:47 -0300

    jogo memoria parte 6 imagens aparecem no site

--- javascript/jogo_da_memoria.js
+++ javascript/jogo_da_memoria.js
@@ -1,7 +1,7 @@
 const moves = document.getElementById("contador-movimento")
 const valorTempo = document.getElementById("tempo")
 const botaoComeçar = document.getElementById("start")
-const containerJogo =  document.querySelector("game-container")
+const containerJogo =  document.querySelector(".game-container")
 const resul = document.getElementById("resultado")
 const controles = document.querySelector(".container-de-controle")
 let cartas
@@ -16,31 +16,34 @@ const itens = [
     {name: "barco", image: "barco.jpg"},
     {name: "bela-imagem", image: "belissima.jpg"},
     {name: "carro", image: "cars.jpg"},
-    {name: "cavalo", image: "CAVALO.jpg"},
+    {name: "cavalo", image: "cavalo.jpg"},
     {name: "vibora", image: "cobra.jpg"},
-    {name: "CRUZ", image: "Cruz.jpg"},
+    {name: "cruz", image: "cruz.png"},
     {name: "escola", image: "ecoola.jpg"},
     {name: "leao", image: "leon.jpg"},
     {name: "peixe", image: "peixe.jpg"},
     {name: "aviao", image: "plane.jpg"},
-    {name: "vaca", image: "VACA.jpg"},
+    {name: "vaca", image: "vaca.png"}
 ]
 
 //tempo inicial
-
 let seconds = 0
   minutes = 0
+
 // movimentos iniciais e contagem de vitórias
 let contMovimentaçao = 0
   contagemVitoria = 0
+
 // for tempo
 const geradorTempo = () =>{
     seconds+=1
+
     //logica dos minutos
     if (seconds >= 60){
         minutes += 1
         seconds = 0
     }
+
 //formato do tempo de mostrar na tela
 let valorSegundos = seconds < 10 ? `0${seconds}` :
 seconds
@@ -54,20 +57,26 @@ const contadorMovimentos = () =>{
     quantidadeMovimento += 1
     moves.innerHTML = `<span>Movimentos:</span>${quantidadeMovimento}`
 }
+
 //pegar um objeto aleatório no array de item 
 const gerarRandom = (size = 4) =>{
+
     //array temporário
     let arrayTemp = [...itens]
+
     //inicializa o array do valor da carta 
     let valorCartas = []
+
     //O tamanho deve ser o dobro (4*4 matrix)/2 estes pares de objetos devem existir
     size = (size*size) / 2
+
     //seletor de um objeto aleátorio
     for (let i = 0; i < size; i++){
         const indexAleatorio = Math.floor(Math.random() * arrayTemp.length)
         valorCartas.push(arrayTemp[indexAleatorio])
+
         //uma vez selecionado remove o objeto do array temporário
-        tempArray.splice(randomIndex, 1)
+        arrayTemp.splice(indexAleatorio, 1)
     }
     return valorCartas
 }
@@ -75,9 +84,27 @@ const gerarRandom = (size = 4) =>{
 const geradorMatrix = (valorCartas, size = 4) => {
     containerJogo.innerHTML = ""
     valorCartas = [...valorCartas, ...valorCartas]
+
     //simples embaralhamento
     valorCartas.sort(() => Math.random() - 0,5)
     for(let i=0; i<size*size;i++){
-
+containerJogo.innerHTML += `
+<div class="container-carta" data-card-value="${valorCartas[i].name}"
+<div class="carta-antes">?</div>
+<div class="carta-depois">
+<img src="./imagens_jogo_da_memória/${valorCartas[i].image}" class="imagem"/></div>
+</div>
+`;
     }
 }
+
+//inicia valores e calculos de funções
+const iniciar = () => {
+   resul.innerText = ""
+   contadorVitoria = 0
+   let valorCartas = gerarRandom()
+   console.log(valorCartas)
+   geradorMatrix(valorCartas)
+}
+
+iniciar();

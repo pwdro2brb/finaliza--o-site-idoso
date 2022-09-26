8c2b8ff07f6853f30375fd9eaef1a765a8a47c43
Pedro Henrique Soares <phenriquesoares360@gmail.com>
2022-09-26 15:22:39 -0300

    TLC parte 6 começo do javascript

---
+++ javascript/script_telas_login_cadastro.js
@@ -0,0 +1,40 @@
+//Nome do tutor
+
+let tutor_nome_input = document.getElementById("input-nome-tutor")
+let erro_tutor = document.getElementById("erro-nome-tutor")
+let vazio_tutor = document.getElementById("vazio-nome-tutor")
+
+//Nome do idoso
+
+let idoso_nome_input = document.getElementById("nome-idoso")
+let erro_idoso = document.getElementById("erro-nome-idoso")
+let vazio_idoso = document.getElementById("vazio-nome-idoso")
+
+//Email
+
+let email_input = document.getElementById("Email")
+let erro_email = document.getElementById("Erro-email")
+let vazio_email = document.getElementById("Email-vazio")
+
+//Telefone do tutor
+
+let telefone_input = document.getComputedStyle("telefone")
+let erro_telefone = document.getElementById("Erro-telefone")
+let vazio_telefone = document.getElementById("telefone-vazio")
+
+//Senha para o cadastro
+
+let senha_input = document.getElementById("senha")
+let erro_senha = document.getElementById("Erro-senha")
+let vazio_senha = document.getElementById("senha-vazia")
+
+//Confirmação da senha
+
+let confimar_senha_input = document.getElementById("verificar-senha")
+let erro_confirmar_senha = document.getElementById("Erro-verificar-senha")
+let vazio_confirmar_senha = document.getElementById("verificar-senha-vazio")
+
+//Botão enviar cadastro
+
+let cadastrar_botao = document.getElementById("botao-cadastrar")
+
